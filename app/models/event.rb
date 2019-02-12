6d8a189c6cd1109acf61c3d7c195d4d7e9712f02
Rarimalalamirana <raharimalalamirana7@gmail.com>
2019-02-12 09:55:21 +0300

    ajout du basede donne et mailer

---
+++ app/models/event.rb
@@ -0,0 +1,11 @@
+class Event < ApplicationRecord
+	belongs_to :user
+	validates :title, presence: true,length:{ in:5..140}
+	validates :description, presence: true ,length:{ in:20..1000}
+	validates :start_date,presence:true
+	validates :price ,presence:true 
+	validates :location ,presence:true 
+	  has_many :attendances
+      has_many :users, through: :attendances
+
+end
